2e946b2b82d6720577e68b7eb0800ba7351483b0
horusq97 <horus.pc97@gmail.com>
2026-02-02 10:09:41 +0200

    02.02

---
+++ cikls_for_un_while.py
@@ -0,0 +1,35 @@
+#cikls - for
+
+#for – ja ir zināms, cik reižu vai pa kādiem elementiem jāatkārto darbība
+#📌 for ar skaitļiem (number)
+for i in range(5): #range() - ļauj apstradāt diapazonu
+    print(i)
+
+#📌 for ar tekstu (string)
+text = "Python"
+
+for letter in text:
+    print(letter)
+
+#📌 for ar sarakstu (list)
+numbers = [3, 7, 2, 9]
+
+for n in numbers:
+    print(n * 2)
+
+#📌 for ar vārdnīcu (dictionary)
+student = {
+    "vārds": "Anna",
+    "vecums": 16,
+    "kurss": "Programmēšana"
+}
+print(student.items())
+for key, value in student.items():
+    print(key, ":", value)
+
+#📌 for ar nosacījumu
+print(2%2)
+print(9%2)
+for i in range(1, 11):
+    if i % 2 == 0: #% - dalījums bez atlikuma
+        print(i, "ir pāra skaitlis")
