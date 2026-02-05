c8d323cf8389e4590130fa832eec4a37e0d33c84
horusq97 <horus.pc97@gmail.com>
2026-02-05 09:56:44 +0200

    05.02

---
+++ cikls_while.py
@@ -0,0 +1,15 @@
+#05.02.2026
+
+#cikls while
+#while ar skaititāju (number)
+skaitlis = 0
+while skaitlis < 5:
+    print(skaitlis)
+    skaitlis += 1 #skaitlis = skaitlis + 1
+
+
+parole = ""
+while parole != "1234":
+    parole = input("Ievadi savu paroli: ")
+
+print("Pareiza parole!")
