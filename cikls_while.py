04ef8dc21c6b315f5ca2f877073f933702f2b6ff
horusq97 <horus.pc97@gmail.com>
2026-02-05 11:01:15 +0200

    05.02.funkcijas

--- cikls_while.py
+++ cikls_while.py
@@ -13,3 +13,25 @@ while parole != "1234":
     parole = input("Ievadi savu paroli: ")
 
 print("Pareiza parole!")
+
+
+#break un continue
+for i in range(10):
+    print(i)
+    if i == 5:
+        break #pārtauc cikla darbību
+print(i)
+
+skaitlis = 0
+while skaitlis < 5:
+    print(skaitlis)
+    skaitlis += 1 #skaitlis = skaitlis + 1
+    if skaitlis == 3:
+        break
+
+#continue
+#continue - izlaiž vienu atkārtojumu
+for i in range(5):
+    if i ==2:
+        continue #ilaiž vienu atkārtojumu
+    print(i)
