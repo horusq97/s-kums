d6824c18477d0fef48cf9e07954cd07362b0e2a7
horusq97 <horus.pc97@gmail.com>
2026-05-14 11:00:34 +0300

    ieskaite 18.05

---
+++ Ieskaite 18.05.py
@@ -0,0 +1,26 @@
+#1.UZD
+
+#Numur 1. ir kvalitatīvi dati, jo visa informācija ir pilna un aizdpildīta kartīgi
+
+#1.2.Uzd
+
+#Marta,16,Jelgava
+
+#2.UZD
+
+skaitlis = input("ievadi vecumu")
+print(skaitlis)
+
+if skaitlis.isdigit():
+    print("ir skaitlis")
+
+    if 0<int(skaitlis) <= 10:
+        print("skaitlis no 1-10")
+    else:
+        print("skaitlis nav 1-10")
+
+#2.2 Uzd
+#Validācija ir svarīga, jo viņa palīdz parbaudīt dotos datus
+
+#3.UZD
+
